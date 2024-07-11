3c640ac4ff870b1b1f3542a6cb40583c58f48745
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-11 12:20:42 -0400

    add port changes and readme update

--- backend/index.ts
+++ backend/index.ts
@@ -7,8 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000, http://localhost:3000');
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT);
 });
 
 
